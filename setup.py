d595cbb88e457ad7ce50eaab4c683dde5319c4ce
Blueblueblob <adrien.lesot@gmail.com>
2020-03-04 10:07:25 +0000

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pygrocydm",
-    version="0.2.0",
+    version="0.2.1",
     author="Blueblueblob",
     description="",
     long_description=long_description,
